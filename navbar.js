62dffc3f5901095da8725332d6faf034465d8c18
POOJA_GK <71807131+arohi146@users.noreply.github.com>
2021-11-10 21:52:11 +0530

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,11 @@
+let navbar=document.getElementById("navbar");
+
+navbar.innerHTML=`
+<nav>
+        <a href="">Home</a>
+        <a href="">Products</a>
+        <a href="">Services</a>
+        <a href="">Portfolio</a>
+        <a href="">Contactus</a>
+    </nav>
+`
